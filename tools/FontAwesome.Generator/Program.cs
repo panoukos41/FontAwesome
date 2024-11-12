75378e319394b3c8e2c18651e112e3d7a38632a7
Panos <panos_ath@outlook.com>
2024-11-12 22:02:45 +0200

    Add .NET 9, update version and fix generator paths

--- tools/FontAwesome.Generator/Program.cs
+++ tools/FontAwesome.Generator/Program.cs
@@ -3,10 +3,19 @@ using P41.FontAwesome.Generator;
 using System.IO.Compression;
 using System.Text;
 
+if (args is not [var fontAwesomeDir])
+{
+    throw new ArgumentException("Expected first argument as FontAwesome.csproj root dir path.");
+}
+
 var currentDir = Environment.CurrentDirectory;
-using var http = new FlurlClient().WithHeader("User-Agent", "request");
+
+// Keep zip and set out dirs
+var outDir = Path.Combine(fontAwesomeDir, "Icons");
+outDir = Path.GetFullPath(outDir);
 
 // Download latest
+using var http = new FlurlClient().WithHeader("User-Agent", "request");
 var fontAwesomeLatest = await http.Request("https://api.github.com/repos/FortAwesome/Font-Awesome/releases/latest").GetJsonAsync<GitHubReleaseResponse>();
 var downloadAsset = fontAwesomeLatest!.Assets.First(x => x.Name.Contains("-web"));
 var zipPath = await http.Request(downloadAsset.BrowserDownloadUrl).DownloadFileAsync(currentDir, downloadAsset.Name);
@@ -15,9 +24,7 @@ var zipPath = await http.Request(downloadAsset.BrowserDownloadUrl).DownloadFileA
 using var zip = new ZipArchive(File.OpenRead(zipPath));
 zip.ExtractToDirectory(currentDir, true);
 
-// Keep zip and set out dirs
 var zipOutDir = Path.Combine(currentDir, Path.GetFileNameWithoutExtension(downloadAsset.Name));
-var outDir = Path.Combine(currentDir, "..", "..", "src", "FontAwesome", "Icons");
 
 // Generate files for all svgs in the zip
 foreach (var directory in Directory.GetDirectories(Path.Combine(zipOutDir, "svgs")))
